48baa0cbb6bb0d37ad4b6dfb11ad580958f5013f
erdemgocen44 <78229631+erdemgocen44@users.noreply.github.com>
2022-01-14 19:57:01 +0100

    class

--- src/main/java/com/techproed/java_dev_summer_tr/SecurityConfigurer.java
+++ src/main/java/com/techproed/java_dev_summer_tr/SecurityConfigurer.java
@@ -1,5 +1,23 @@
 package com.techproed.java_dev_summer_tr;
 
-public class SecurityConfigurer {
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
+
+@EnableWebSecurity
+public class SecurityConfigurer extends WebSecurityConfigurerAdapter{
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+
+		PasswordEncoder encoder=PasswordEncoderFactories.createDelegatingPasswordEncoder();
+		
+		auth.inMemoryAuthentication().withUser("admin").password(encoder.encode("nimda")).roles("ADMIN");
+	}
+
+	
+	
 }
